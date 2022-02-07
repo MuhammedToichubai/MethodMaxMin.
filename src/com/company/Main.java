4354afdd3b4c75cb1f26a37d236269214decd6f7
Muhammed <muhammedtoichubai@gmail.com>
2022-02-07 00:27:39 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//    int a = 0;
+//    while (a<10){
+//        System.out.println("Hello Joomart");
+//      a++; //a+1 =1(0+1=1)
+
+        //       Scanner muh = new Scanner(System.in);
+//        int a = muh.nextInt();
+//        int scetchik = 0 ;
+//        while (a > 0) {
+//            a /= 10;
+//            scetchik++;
+//        }
+//        System.out.println(scetchik);
+
+
+//        int bir = muh.nextInt();
+//        int eki = muh.nextInt();
+//        bir++;
+//        while (bir < eki){
+//            System.out.println(bir*bir);
+        //          bir++;
+
+
+//
+//        int san = muh.nextInt();
+//        san = 1;
+//        san++;
+//        while (0 < san) {
+
+        //       public static void main (String[]args){
+// //           int[] maanisi = new int[]{65, 89, 94, 95, 97, 2000, 68};
+//
+//        }
+////             static void metodChon(int[] maanisi){
+////             int summa = maanisi[0];
+////            for (int i = 0; i < maanisi.length; i++) {
+////                if (maanisi[i] >= summa){
+////                    summa = maanisi[i];
+////                }
+////            }
+////            System.out.println("Massivinizdegi en chon san - "+summa);
+////        }
+////        static void metodKichine(int[] maanisi){
+////            int summa = maanisi[0];
+////            for (int i = 0; i < maanisi.length; i++) {
+////                if (maanisi[i] <= summa){
+////                    summa = maanisi[i];
+////                }
+//////            }
+//////            System.out.println("Massivinizdegi en kichine san - "+summa);
+//        }
+
+
+    }
+}
